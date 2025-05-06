e089e0de398a48f6164a7100903b1d608becf493
cromanpa94 <cromanpa94@gmail.com>
2025-05-05 20:42:25 -0700

    panels

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(DT)
 
-# Initialize data storage
+# Data storage
 responses <- reactiveVal(data.frame(
   ID = numeric(0),
   Answer = character(0),
@@ -9,40 +9,53 @@ responses <- reactiveVal(data.frame(
   stringsAsFactors = FALSE
 ))
 
-password <- "conference123" # Set your desired password here
+password <- "conference1"
 
 ui <- fluidPage(
   titlePanel("Conference interactive survey"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput("answer", "Enter your idea:"),
-      actionButton("submit", "Submit"),
-      hr(),
-      passwordInput("pwd", "Enter password to restart survey:"),
-      actionButton("restart", "Restart survey")
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Vote", 
+  tabsetPanel(
+    tabPanel("Guest panel",
+             sidebarLayout(
+               sidebarPanel(
+                 textInput("answer", "Enter your idea:"),
+                 actionButton("submit", "Submit")
+               ),
+               mainPanel(
                  h4("Vote for existing ideas:"),
-                 DTOutput("vote_table")
-        ),
-        tabPanel("Top ideas",
-                 h4("Top Ideas by votes"),
-                 DTOutput("results_table")
-        )
-      )
+                 DTOutput("vote_table_public")
+               )
+             )
+    ),
+    tabPanel("Admin panel",
+             sidebarLayout(
+               sidebarPanel(
+                 passwordInput("admin_pwd", "Admin password:"),
+                 conditionalPanel(
+                   condition = "input.admin_pwd == 'conference1'",
+                   actionButton("restart", "Restart survey")
+                 )
+               ),
+               mainPanel(
+                 conditionalPanel(
+                   condition = "input.admin_pwd == 'conference1'",
+                   h4("Admin overview:"),
+                   DTOutput("admin_table")
+                 )
+               )
+             )
     )
   )
 )
 
 server <- function(input, output, session) {
+  user_votes <- reactiveVal(integer())
+  
   # Submit a new answer
   observeEvent(input$submit, {
     req(input$answer)
     current <- responses()
     new_entry <- data.frame(
-      ID = ifelse(nrow(current)==0, 1, max(current$ID) + 1),
+      ID = ifelse(nrow(current) == 0, 1, max(current$ID) + 1),
       Answer = input$answer,
       Votes = 0,
       stringsAsFactors = FALSE
@@ -53,29 +66,39 @@ server <- function(input, output, session) {
   
   # Restart survey
   observeEvent(input$restart, {
-    req(input$pwd == password)
+    req(input$admin_pwd == password)
     responses(data.frame(ID = numeric(0), Answer = character(0), Votes = numeric(0), stringsAsFactors = FALSE))
-    updateTextInput(session, "pwd", value = "")
+    user_votes(integer())
+    updateTextInput(session, "admin_pwd", value = "")
   })
   
-  # Display voting table
-  output$vote_table <- renderDT({
-    datatable(responses()[, c("ID", "Answer", "Votes")], selection = "single", rownames = FALSE)
+  # Public voting table
+  output$vote_table_public <- renderDT({
+    current <- responses()
+    votes_by_user <- user_votes()
+    current$YourVotes <- votes_by_user[as.character(current$ID)]
+    current$YourVotes[is.na(current$YourVotes)] <- 0
+    datatable(current[, c("Answer", "YourVotes")], selection = "single", rownames = FALSE, options = list(dom = 't'))
   })
   
-  # Voting action
-  observeEvent(input$vote_table_rows_selected, {
-    selected <- input$vote_table_rows_selected
+  # User voting action
+  observeEvent(input$vote_table_public_rows_selected, {
+    selected <- input$vote_table_public_rows_selected
     if(length(selected)){
       current <- responses()
       current$Votes[selected] <- current$Votes[selected] + 1
       responses(current)
+      
+      votes_by_user <- user_votes()
+      id_char <- as.character(current$ID[selected])
+      votes_by_user[id_char] <- ifelse(is.na(votes_by_user[id_char]), 1, votes_by_user[id_char] + 1)
+      user_votes(votes_by_user)
     }
   })
   
-  # Display results
-  output$results_table <- renderDT({
-    datatable(responses()[order(-responses()$Votes), c("Answer", "Votes")], rownames = FALSE)
+  # Admin table
+  output$admin_table <- renderDT({
+    datatable(responses()[order(-responses()$Votes), -1], rownames = FALSE)
   })
 }
 
